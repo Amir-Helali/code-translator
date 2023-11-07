4612f92ac56f884e890795b0881da2b290497113
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2023-11-07 11:06:37 -0600

    added language selectors

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -23,6 +23,10 @@ export default function Index() {
   const [selectedModel, setSelectedModel] = useState<"gpt-3.5" | "gpt-4">(
     "gpt-3.5"
   );
+  const [inputLanguage, setInputLanguage] = useState<string>("");
+  const [outputLanguage, setOutputLanguage] = useState<string>("");
+
+  const languages = ["python", "javascript", "java"];
 
   return (
     <>
@@ -82,15 +86,83 @@ export default function Index() {
       </Flex>
       <Flex justify={"center"} gap={"4"}>
         <Card>
-          <Text size={"2"} align={"center"} as="div">
-            Input
-          </Text>
+          <Flex justify={"center"}>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Button variant="soft">
+                  Input
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </Button>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <>
+                  {languages.map((language, index) => {
+                    return (
+                      <DropdownMenu.Item
+                        onClick={() => setInputLanguage(language)}
+                        key={`input-${language}-${index}`}
+                      >
+                        {language}
+                      </DropdownMenu.Item>
+                    );
+                  })}
+                </>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          </Flex>
           <TextArea style={{ minHeight: "60vh", minWidth: "30vw" }} />
         </Card>
         <Card>
-          <Text size={"2"} align={"center"} as="div">
-            Output
-          </Text>
+          <Flex justify={"center"}>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Button variant="soft">
+                  Output
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </Button>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <>
+                  {languages.map((language, index) => {
+                    return (
+                      <DropdownMenu.Item
+                        onClick={() => setOutputLanguage(language)}
+                        key={`output-${language}-${index}`}
+                      >
+                        {language}
+                      </DropdownMenu.Item>
+                    );
+                  })}
+                </>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          </Flex>
           <TextArea style={{ minHeight: "60vh", minWidth: "30vw" }} />
         </Card>
       </Flex>
